aa98227d910bfc7175eed5bf21ad89e7495667a2
Alessandro <alessandrocosta103@gmail.com>
2023-05-05 20:58:20 -0300

    cria pacote model

---
+++ app/src/main/java/model/project.java
@@ -0,0 +1,12 @@
+package model;
+
+public class project {
+    private int id;
+    private String name;
+    private String description;
+    private String createdAt;
+    private String updatedAt;
+    
+    public project(){};
+    
+}
---
+++ app/src/main/java/model/task.java
@@ -0,0 +1,16 @@
+
+package model;
+
+public class task {
+    private int id;
+    private int idProject;
+    private String name;
+    private String description;
+    private Boolean completed;
+    private String notes;
+    private String deadline;
+    private String createdAt;
+    private String updatedAt;
+    
+    public task(){};
+}
