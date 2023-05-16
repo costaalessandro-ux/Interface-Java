09f4f0513c087d6e8f533020479d8b8505acef42
Alessandro <alessandrocosta103@gmail.com>
2023-05-16 14:21:54 -0300

    cria controller

---
+++ app/src/main/java/controller/ProjectController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import model.Project;
+
+public class ProjectController {
+     public void save(Project project){
+        
+    }
+    
+    public void update(Project project){
+        
+    }
+    
+    public void removeById(Project project){
+        String sql = "DELETE FROM task where id = ?";
+    }
+    
+}
--- app/src/main/java/controller/TaskController.java
+++ app/src/main/java/controller/TaskController.java
@@ -1,7 +1,11 @@
 
 package controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.ConnectionFactory;
 import model.Task;
 
 public class TaskController {
@@ -14,8 +18,18 @@ public class TaskController {
         
     }
     
-    public void removeById(Task task){
+    public void removeById(int taskId) throws SQLException{
         String sql = "DELETE FROM task where id = ?";
+        Connection conn = null;
+        PreparedStatement statement = null;
+        try{
+           conn = ConnectionFactory.getConnection();
+           statement = conn.prepareStatement(sql);
+           statement.setInt(1, taskId);
+           statement.execute();
+        }catch (SQLException e){
+            throw new SQLException("Error ao deletar tarefa");
+        }
     }
     
     public List<Task> getAll(int idProject){
