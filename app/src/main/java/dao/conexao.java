e1734798d109824b4b43100734d7c132e9fc2f66
Alessandro <alessandrocosta103@gmail.com>
2023-05-26 23:52:38 -0300

    upgrade

--- app/src/main/java/dao/conexao.java
+++ app/src/main/java/dao/conexao.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 
 public class conexao {
     private Connection conexao;
-    public static final String DRIVER = "com.mysql.jdbc.Driver";
-    public static final String URL = "jdbc:mysql://localhost:3306/todolist";
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://127.0.0.1:3307/tolist";
     public static final String USER = "root";
-    public static final String PASS = "";
+    public static final String PASS = "root";
     
     public Connection getConnection(){
         try{
