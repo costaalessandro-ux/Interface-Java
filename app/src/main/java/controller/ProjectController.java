d5b8917e9eafa748f86cbdebe495c958596d579a
Alessandro <alessandrocosta103@gmail.com>
2023-06-06 18:46:55 -0300

    upgrade

--- app/src/main/java/controller/ProjectController.java
+++ app/src/main/java/controller/ProjectController.java
@@ -1,18 +1,84 @@
 package controller;
 
+import dao.conexao;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import model.Project;
 
 public class ProjectController {
-     public void save(Project project){
-        
+
+    private Connection conexao; //conexao.
+    private PreparedStatement preparar; // preparar o SQL.
+    private String sql;
+    private ResultSet rs;
+
+    public ProjectController() throws ClassNotFoundException, SQLException {
+        conexao = new conexao().getConnection();
     }
-    
-    public void update(Project project){
-        
+
+    public void save(Project project) throws SQLException {
+        sql = "insert into project (id, name, description, createdAt, updatedAt) values (?,?,?,?,?)";
+        preparar = conexao.prepareStatement(sql);
+        try {
+            preparar.setInt(1, project.getId());
+            preparar.setString(2, project.getName());
+            preparar.setString(3, project.getDescription());
+            preparar.setDate(4, (Date) project.getCreatedAt());
+            preparar.setDate(5, (Date) project.getUpdatedAt());
+            preparar.execute();
+            preparar.close();
+        } catch (SQLException e) {
+            throw new SQLException("Error ao inserir um Projeto");
+        }
+
     }
-    
-    public void removeById(Project project){
-        String sql = "DELETE FROM task where id = ?";
+
+    public void update(Project project) throws SQLException {
+        sql = "update Project project set name=?, description=?, notes=?, deadline=?, createdAt=?, updatedAt=? where id=?";
+        preparar = conexao.prepareStatement(sql);
+        try {
+            preparar.setString(1, project.getName());
+            preparar.setString(2, project.getDescription());
+            preparar.setDate(3, (Date) project.getCreatedAt());
+            preparar.setDate(4, (Date) project.getUpdatedAt());
+            preparar.execute();
+            preparar.close();
+        } catch (SQLException e) {
+            throw new SQLException("Error ao alterar Projeto");
+        }
+
     }
-    
+
+    public void removeById(int idProject) throws SQLException {
+        sql = "DELETE FROM project where id = ?";
+        preparar = conexao.prepareStatement(sql);
+        try {
+            preparar.setInt(1, idProject);
+            preparar.execute();
+            preparar.close();
+        } catch (SQLException e) {
+            throw new SQLException("Error ao deletar o projeto");
+        }
+    }
+
+    public List<Project> getAll(int id) throws SQLException {
+        List<Project> projects = new ArrayList<Project>();
+        sql = "select * from project order by id";
+        preparar = conexao.prepareStatement(sql);
+        rs = preparar.executeQuery();
+        while (rs.next()) {
+            Project project = new Project();
+            project.setId(rs.getInt("id"));
+            project.setName(rs.getString("name"));
+            project.setDescription(rs.getString("description"));
+            project.setCreatedAt(rs.getDate("createdAt"));
+            project.setUpdatedAt(rs.getDate("updatedAt"));
+        }
+        return null;
+    }
 }
--- app/src/main/java/controller/TaskController.java
+++ app/src/main/java/controller/TaskController.java
@@ -1,4 +1,3 @@
-
 package controller;
 
 import dao.conexao;
@@ -12,76 +11,85 @@ import java.util.List;
 import model.Task;
 
 public class TaskController {
-    
-        private Connection conexao; //conexao.
-	private PreparedStatement preparar; // preparar o SQL.
-	private String sql;
-	private ResultSet rs;
-     
-    public TaskController () throws ClassNotFoundException, SQLException {
-		conexao = new conexao().getConnection();
+
+    private Connection conexao; //conexao.
+    private PreparedStatement preparar; // preparar o SQL.
+    private String sql;
+    private ResultSet rs;
+
+    public TaskController() throws ClassNotFoundException, SQLException {
+        conexao = new conexao().getConnection();
     }
-        
-    public void save(Task task) throws SQLException{
+
+    public void save(Task task) throws SQLException {
         sql = "insert into task (idProject,name,description,completed,notes,deadline,createdAt,updatedAt) values(?,?,?,?,?,?,?,?)";
         preparar = conexao.prepareStatement(sql);
-        preparar.setInt(1,task.getIdProject());
-        preparar.setString(2,task.getName());
-        preparar.setString(3,task.getDescription());
-        preparar.setBoolean(4,task.getCompleted());
-        preparar.setString(5,task.getNotes());
-        preparar.setString(6,task.getDeadline());
-        preparar.setDate(7, (Date) task.getCreatedAt());
-        preparar.setDate(8, (Date) task.getUpdatedAt());
-        preparar.execute();
-        preparar.close();
+        try {
+            preparar.setInt(1, task.getIdProject());
+            preparar.setString(2, task.getName());
+            preparar.setString(3, task.getDescription());
+            preparar.setBoolean(4, task.getCompleted());
+            preparar.setString(5, task.getNotes());
+            preparar.setString(6, task.getDeadline());
+            preparar.setDate(7, (Date) task.getCreatedAt());
+            preparar.setDate(8, (Date) task.getUpdatedAt());
+            preparar.execute();
+            preparar.close();
+        } catch (SQLException e) {
+            throw new SQLException("Error ao inserir a tarefa");
+        }
+
     }
-   
-    public void update(Task task) throws SQLException{
-        sql = "update Task task set name=?, description=?, completed=?, notes=?, deadline=?, createdAt=?, updateAt=? where id=?";
+
+    public void update(Task task) throws SQLException {
+        sql = "update Task task set name=?, description=?, completed=?, notes=?, deadline=?, createdAt=?, updatedAt=? where id=?";
         preparar = conexao.prepareStatement(sql);
-        preparar.setString(1,task.getName());
-        preparar.setString(2,task.getDescription());
-        preparar.setBoolean(3,task.getCompleted());
-        preparar.setString(4,task.getNotes());
-        preparar.setString(5,task.getDeadline());
-        preparar.setDate(6, (Date) task.getCreatedAt());
-        preparar.setDate(7, (Date) task.getUpdatedAt());
-        preparar.execute();
-        preparar.close();
+        try {
+            preparar.setString(1, task.getName());
+            preparar.setString(2, task.getDescription());
+            preparar.setBoolean(3, task.getCompleted());
+            preparar.setString(4, task.getNotes());
+            preparar.setString(5, task.getDeadline());
+            preparar.setDate(6, (Date) task.getCreatedAt());
+            preparar.setDate(7, (Date) task.getUpdatedAt());
+            preparar.execute();
+            preparar.close();
+        } catch (SQLException e) {
+            throw new SQLException("Error ao alterar a tarefa");
+        }
     }
-    
-    public void removeById(int taskId) throws SQLException{
+
+    public void removeById(int taskId) throws SQLException {
         sql = "DELETE FROM task where id = ?";
         preparar = conexao.prepareStatement(sql);
-        try{
-           preparar.setInt(1, taskId);
-           preparar.execute();
-           preparar.close();
-        }catch (SQLException e){
+        try {
+            preparar.setInt(1, taskId);
+            preparar.execute();
+            preparar.close();
+        } catch (SQLException e) {
             throw new SQLException("Error ao deletar tarefa");
         }
     }
-    
-    public List<Task> getAll(int idProject) throws SQLException{
-        List<Task> tasks = new ArrayList<Task>(); 
+
+    public List<Task> getAll(int idProject) throws SQLException {
+        List<Task> tasks = new ArrayList<Task>();
         sql = "select * from task order by idProject";
         preparar = conexao.prepareStatement(sql);
         //preparar.setInt(1, idProject);
         rs = preparar.executeQuery();
-        while(rs.next()) {
-	Task task = new Task();
-        task.setId(rs.getInt("id"));
-        task.setIdProject(rs.getInt("idProject"));
-        task.setName(rs.getString("name"));
-        task.setDescription(rs.getString("description"));
-        task.setCompleted(rs.getBoolean("completed"));
-        task.setNotes(rs.getString("notes"));
-        task.setDeadline(rs.getString("deadline"));
-        task.setCreatedAt(rs.getDate("createdAt"));
-        task.setUpdatedAt(rs.getDate("updatedAt"));
-        tasks.add(task);
-	}
+        while (rs.next()) {
+            Task task = new Task();
+            task.setId(rs.getInt("id"));
+            task.setIdProject(rs.getInt("idProject"));
+            task.setName(rs.getString("name"));
+            task.setDescription(rs.getString("description"));
+            task.setCompleted(rs.getBoolean("completed"));
+            task.setNotes(rs.getString("notes"));
+            task.setDeadline(rs.getString("deadline"));
+            task.setCreatedAt(rs.getDate("createdAt"));
+            task.setUpdatedAt(rs.getDate("updatedAt"));
+            tasks.add(task);
+        }
         return null;
     }
 }
